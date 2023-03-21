9dc9408ee120eb3e0f24700c2bb9e94172525098
khalilmohammadmirza <khalilmohammadmirza@gmail.com>
2023-03-21 09:26:31 +0500

    Extract PartitionKey Logic to seperate function

--- dpk.js
+++ dpk.js
@@ -1,29 +1,13 @@
 const { createHash } = require("crypto");
 
 const deterministicPartitionKey = (event) => {
-  const MAX_PARTITION_KEY_LENGTH = 256;
   let candidate;
 
   // Check for empty event and simply return TRIVIAL_PARTITION_KEY if empty
   if (event) {
     // Check for partitionKey
     if (event.partitionKey) {
-      candidate = event.partitionKey;
-
-      // Canidate is always going to be non string for events without partitionKey
-      // as we were creating hash first for non partitionKey events
-      // so we can simplify logic by only checking this for partitionKey events
-      if (typeof candidate !== "string") {
-        candidate = JSON.stringify(candidate);
-      }
-
-      // for events without partitionKey
-      // as we are creating hash
-      // Sha3-512 hex hash always has length of 128
-      // so we can simplify logic by moving this check for partitionKey
-      if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-        candidate = createHash("sha3-512").update(candidate).digest("hex");
-      }
+      candidate = handlePartitionKey(event.partitionKey);
     } else {
       // If there is no partitionKey, we are always creating hash of event so we simplify the logic
       const data = JSON.stringify(event);
@@ -36,5 +20,27 @@ const deterministicPartitionKey = (event) => {
   // return TRIVIAL_PARTITION_KEY for empty events
   return "0";
 };
+
+// Extract PartitionKey Logic to seperate function
+function handlePartitionKey(candidate) {
+  const MAX_PARTITION_KEY_LENGTH = 256;
+
+  // Canidate is always going to be non string for events without partitionKey
+  // as we were creating hash first for non partitionKey events
+  // so we can simplify logic by only checking this for partitionKey events
+  if (typeof candidate !== "string") {
+    candidate = JSON.stringify(candidate);
+  }
+
+  // for events without partitionKey
+  // as we are creating hash
+  // Sha3-512 hex hash always has length of 128
+  // so we can simplify logic by moving this check for partitionKey
+  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
+    candidate = createHash("sha3-512").update(candidate).digest("hex");
+  }
+
+  return candidate;
+}
 
 exports.deterministicPartitionKey = deterministicPartitionKey;
